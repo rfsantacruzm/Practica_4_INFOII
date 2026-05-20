27210d811976738f0650da11ec9df9219a256c26
rfsantacruzm <francisco.santacruz@udea.edu.co>
2026-05-19 22:03:45 -0500

    Actualizacion network (metodos carga y generacion)

--- Practica4/network.cpp
+++ Practica4/network.cpp
@@ -1,6 +1,9 @@
 #include "network.h"
 #include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <random>
 
 using namespace std;
 
@@ -25,6 +28,63 @@ void Network::removeRouter(Router* router) {
         r->routingTable.erase(router);
     }
     delete router;
+}
+
+void Network::loadTopologyFromFile(const string& filename) {
+    ifstream file(filename);
+    string line;
+    while(getline(file, line)) {
+        istringstream iss(line);
+        string router1Name, router2Name;
+        int cost;
+        if(!(iss >> router1Name >> router2Name >> cost)) {
+            break;
+        }
+        Router* router1 = findOrCreateRouter(router1Name);
+        Router* router2 = findOrCreateRouter(router2Name);
+        router1->updateRoutingTable(router2, cost);
+        router2->updateRoutingTable(router1, cost);
+    }
+    file.close();
+}
+
+Router* Network::findOrCreateRouter(const string& name) {
+    for(auto& router : routers) {
+        if(router->name == name) {
+            return router;
+        }
+    }
+    Router* newRouter = new Router(name);
+    routers.push_back(newRouter);
+    return newRouter;
+}
+
+void Network::generateRandomNetwork(int numRouters, int maxCost) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distr(1, maxCost);
+    uniform_int_distribution<> distr_bool(0, 1);
+    for(int i = 0; i < numRouters; i++) {
+        Router* newRouter = new Router("Router" + to_string(i));
+        routers.push_back(newRouter);
+    }
+    for(int i = 0; i < numRouters; i++) {
+        // Aseguramos que al menos un enrutador esté conectado
+        int j = distr(gen) % numRouters;
+        if(j != i) {
+            int cost = distr(gen);
+            routers[i]->updateRoutingTable(routers[j], cost);
+            routers[j]->updateRoutingTable(routers[i], cost);
+        }
+        // Para el resto de enrutadores, decidimos aleatoriamente si conectarlos o no
+        for(int j = i + 1; j < numRouters; j++) {
+            if(distr_bool(gen)) {
+                int cost = distr(gen);
+                routers[i]->updateRoutingTable(routers[j], cost);
+                routers[j]->updateRoutingTable(routers[i], cost);
+            }
+        }
+    }
 }
 
 void Network::printAllRoutingTables() {
--- Practica4/network.h
+++ Practica4/network.h
@@ -13,6 +13,9 @@ public:
     void addRouter(Router* router);
     void removeRouter(Router* router);
     void addConnection(Router* router1, Router* router2, int cost);
+    void loadTopologyFromFile(const std::string& filename);
+    Router* findOrCreateRouter(const std::string& name);
+    void generateRandomNetwork(int numRouters, int maxCost);
     void printAllRoutingTables();
 };
 
